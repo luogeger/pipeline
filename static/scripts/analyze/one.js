18dc0eacad0e6cbd533daffadf4cb30894eeab9e
luogeger <xiaoqing1232@163.com>
2018-03-23 13:57:15 +0800

    重新整理数据

--- static/scripts/analyze/one.js
+++ static/scripts/analyze/one.js
@@ -3,31 +3,75 @@ $.ajaxSettings.async = false;// 同步请求
 var objFn;
 var data = {
     results: '',
-    yAxis: [],
-    sTarget: [],
-    sComplete: [],
-    yearTbale: '',
+    chart: {
+        yAxis: [],
+        sTarget: [],
+        sComplete: [],
+    },
+    yearTitle: {},
+    yearTable: {},
+    yearTotal: {},
+    quarterTable: {
+        Q1: [],
+        Q2: [],
+        Q3: [],
+        Q4: [],
+    },
 };
-$.getJSON('../static/json/one.json', function (results) {
+
+$.getJSON('../static/json/one1.json', function (results) {
     data.results = results.msg;
 });
 
-
+//  处理数据
 //  =========================================================
 objFn = {
-    settleData: function (results) {
-        data.yAxis.splice(0, data.yAxis.length);// 清空之前数组
-        data.sTarget.splice(0, data.sTarget.length);// 清空之前数组
-        data.sComplete.splice(0, data.sComplete.length);// 清空之前数组
-        results.half.forEach(function (p1) {
-            data.yAxis.push(p1.saleGroup)
-            data.sTarget.push(p1.target)
-            data.sComplete.push(p1.complete)
+    settleData: function (res) {
+
+        data.chart.yAxis.splice(0,      data.chart.yAxis.length);// 清空之前数组
+        data.chart.sTarget.splice(0,    data.chart.sTarget.length);// 清空之前数组
+        data.chart.sComplete.splice(0,  data.chart.sComplete.length);// 清空之前数组
+        res.half.forEach(function (p1) {
+            data.chart.yAxis.push(p1.saleGroup);
+            data.chart.sTarget.push(p1.target);
+            data.chart.sComplete.push(p1.complete);
         });
-        data.yearTbale = results.half;
+        data.yearTable = res.half;
+        data.yearTitle = res.title;
+
+
+        //  ==  季度数据
+        if (data.results.h1.quarter.q1)
+            data.results.h1.quarter.q1.forEach(function (p1, p2, p3) {
+            data.quarterTable.Q1.push(p1)
+        });
+        if (data.results.h1.quarter.q2)
+            data.results.h1.quarter.q2.forEach(function (p1, p2, p3) {
+            data.quarterTable.Q2.push(p1)
+        });
+        if (data.results.h2.quarter.q3)
+            data.results.h2.quarter.q3.forEach(function (p1, p2, p3) {
+            data.quarterTable.Q3.push(p1)
+        });
+
+        if (data.results.h2.quarter.q4)
+            data.results.h2.quarter.q4.forEach(function (p1, p2, p3) {
+            data.quarterTable.Q4.push(p1)
+        });
+
+
+        // var keyQ;
+        // for(key in data.quarterTable){
+        //     if (data.quarterTable[key].length === 0) {
+        //         data.quarterTable[key]
+        //         keyQ = key;
+        //         delete data.quarterTable[keyQ]
+        //     }
+        // }
+
     },
 
-    half: function (datas) {
+    chart: function () {
         var box = echarts.init(document.getElementById('halfYearChart'));
         option = {
             tooltip: {
@@ -57,7 +101,7 @@ objFn = {
                     fontSize: '16',
                     fontWeight: '600',
                 },
-                data: datas.yAxis
+                data: data.chart.yAxis
             },
             series: [
                 {
@@ -68,8 +112,7 @@ objFn = {
                     itemStyle:{
                         barBorderRadius: [0, 50, 50, 0], //（顺时针左上，右上，右下，左下）
                     },
-                    //data: [18203, 23489, 29034, 104970, 131744]
-                    data: datas.sTarget
+                    data: data.chart.sTarget
                 },
                 {
                     name: '部门完成情况',
@@ -79,8 +122,7 @@ objFn = {
                     itemStyle:{
                         barBorderRadius: [0, 50, 50, 0], //（顺时针左上，右上，右下，左下）
                     },
-                    //data: [19325, 23438, 31000, 121594, 134141]
-                    data: datas.sComplete
+                    data: data.chart.sComplete
                 }
             ]
         };
@@ -96,31 +138,55 @@ objFn = {
 //  =========================================================
 // == init
 objFn.settleData(data.results.h1);
-objFn.half(data);
+objFn.chart(data);
+console.log(data.quarterTable)
+// == 年度表格
 var yearTable = new Vue({
     el: '#yearTable',
     data: {
-        list: data.yearTbale,
+        title: data.yearTitle,
+        list: data.yearTable,
+    }
+});
+
+// == 季度表格
+var quarterTable = new Vue({
+    el: '#quarterTable',
+    data: {
+        list: data.quarterTable.Q1,
+        isActive: 'Q2',
+        obj: data.quarterTable,
+    },
+    methods: {
+        Qevent: function (index) {
+            var _key = 'Q' + (index + 1);
+            this.isActive = _key;
+            this.list = data.quarterTable[_key];
+        }
     }
 });
 
 // == 上半年
 $('#yearHalfFirst').click(function () {
     objFn.settleData(data.results.h1);
-    objFn.half(data);
+    objFn.chart(data.chart);
     $('#yearHalfLast').removeClass('year-half-active');
     $(this).addClass('year-half-active');
-    yearTable.list = data.yearTbale;
+    yearTable.list = data.yearTable;
+    yearTable.title = data.yearTitle;
 });
 
 
 // == 下半年
 $('#yearHalfLast').click(function () {
     objFn.settleData(data.results.h2);
-    objFn.half(data);
+    objFn.chart(data.chart);
     $('#yearHalfFirst').removeClass('year-half-active');
     $(this).addClass('year-half-active');
-    yearTable.list = data.yearTbale;
+    yearTable.list = data.yearTable;
+    yearTable.title = data.yearTitle;
+
+    console.log(data.quarterTable)
 });
 
 
