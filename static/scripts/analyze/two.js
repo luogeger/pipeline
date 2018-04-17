97036abb4392a7e12f4a05cc93b60273674cf73d
luogeger <xiaoqing1232@163.com>
2018-04-17 19:19:29 +0800

    css finish

--- static/scripts/analyze/two.js
+++ static/scripts/analyze/two.js
@@ -5,6 +5,10 @@ laydate.render({
     elem: '#dateOne', //指定元素
     range: true,
 });
+laydate.render({
+    elem: '#dateTwo', //指定元素
+    range: true,
+});
 
 var vm = new Vue({
     ele: '#app',
@@ -14,6 +18,7 @@ var vm = new Vue({
 
     created: function () {
         this.yearChart();
+        this.quarterChart();
     },
 
     methods: {
@@ -45,7 +50,80 @@ var vm = new Vue({
                 },
 
                 grid: {
-                    left: '8%',
+                    left: '5%',
+                    right: '5%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                xAxis: [
+                    {
+                        type: 'category',
+                        axisTick: {show: false},
+                        data: [
+                            '金融客户事业部',
+                            '行业客户事业部',
+                            '通用客户事业部',
+                            '港台客户事业部',
+                            '公共客户事业部',
+                        ]
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value',
+                    }
+                ],
+                series: [
+                    {
+                        name: '目标额',
+                        type: 'bar',
+                        itemStyle:{
+                            barBorderRadius: [5, 5, 0, 0], //（顺时针左上，右上，右下，左下）
+                        },
+                        data: [1200, 2000, 1500, 800, 700,],
+                    },
+                    {
+                        name: '完成额',
+                        type: 'bar',
+                        itemStyle:{
+                            barBorderRadius: [5, 5, 0, 0], //（顺时针左上，右上，右下，左下）
+                        },
+                        data: [3120, 1200, 2150, 980, 370,],
+                    },
+                ]
+            };
+            box.setOption(option);
+        },
+
+        quarterChart: function () {
+            var box = echarts.init(document.getElementById('quarterChart'));
+
+            option = {
+                color: ['#ED6D00', '#FFC732'],
+                barMaxWidth: '20%',// 柱子宽度
+                barGap: 0,// 柱子之间没有间隙
+
+
+                title : {
+                    text: '加权平均额周期对比 (加权平均vs目标) (单位：万元)',
+                    x:'left',
+                    top: '20px',
+                    textStyle: {
+                        fontSize: 14,
+                    }
+                },
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'shadow'
+                    }
+                },
+                legend: {
+                    data: ['目标额', '完成额',]
+                },
+
+                grid: {
+                    left: '5%',
                     right: '5%',
                     bottom: '3%',
                     containLabel: true
