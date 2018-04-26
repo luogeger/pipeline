b2a33cc61f3cafbefd7c358b6bf9730e09b21403
kellyo0 <1281002678@qq.com>
2018-04-26 17:41:55 +0800

    项目更新情况功能重写，项目更新时间显示效果修改；登录，修改密码加密

--- static/scripts/login/login.js
+++ static/scripts/login/login.js
@@ -2,15 +2,55 @@ var vm = new Vue({
     el: '#app',
     data: function(){
         return {
-            loginList: {},
+            loginList: {
+                loginAccount: '',
+                loginPass: ''
+            },
             msg: '',
             PATH: 'http://172.16.8.130:8080/iboss-prism',
             // PATH: '/iboss-prism',
+            keyStr: "ABCDEFGHIJKLMNOP" + "QRSTUVWXYZabcdef" + "ghijklmnopqrstuv"
+            + "wxyz0123456789+/" + "=",
         }
     },
     methods: {
+        // base64加密开始
+        encode64: function(input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+            do {
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+                output = output + this.keyStr.charAt(enc1) + this.keyStr.charAt(enc2)
+                    + this.keyStr.charAt(enc3) + this.keyStr.charAt(enc4);
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+            } while (i < input.length);
+
+            return output;
+        },
+        // base64加密结束
         // 登陆
         submitHandle: function() {
+            var password = this.encode64(vm.loginList.loginPass); // 对数据加密
+            // var password = this.encode64($("#passwordLogin").val()); // 对数据加密
+            console.log(password,'password');
+            vm.loginList.loginPass = password;
+            if(vm.loginList.loginPass == 'AA==') {
+                delete vm.loginList.loginPass;
+            }
             var _this = this;
             $.ajax({
                 url: _this.PATH + '/oauth/login',
--- static/scripts/login/password.js
+++ static/scripts/login/password.js
@@ -7,12 +7,54 @@ var vm = new Vue({
                 newLoginPass: ''
             },
             msg: '',
-            newLoginPassAgain: ''
+            newLoginPassAgain: '',
+            keyStr: "ABCDEFGHIJKLMNOP" + "QRSTUVWXYZabcdef" + "ghijklmnopqrstuv"
+            + "wxyz0123456789+/" + "=",
         }
     },
     methods: {
+        // base64加密开始
+        encode64: function(input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+            do {
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+                output = output + this.keyStr.charAt(enc1) + this.keyStr.charAt(enc2)
+                    + this.keyStr.charAt(enc3) + this.keyStr.charAt(enc4);
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+            } while (i < input.length);
+
+            return output;
+        },
+        // base64加密结束
         submitHandle: function() {
-            if(vm.passwordList.newLoginPass != vm.newLoginPassAgain){
+            var oldPassword = this.encode64(vm.passwordList.oldLoginPass); // 对数据加密
+            var newPassword = this.encode64(vm.passwordList.newPassword); // 对数据加密
+            /*console.log(oldPassword,'password');
+            console.log(newPassword,'password');*/
+            vm.passwordList.oldLoginPass = oldPassword;
+            vm.passwordList.newPassword = newPassword;
+            if(vm.passwordList.oldLoginPass == 'AA==') {
+                delete vm.passwordList.oldLoginPass;
+            }
+            else if(vm.passwordList.newLoginPass == 'AA==') {
+                delete vm.passwordList.newLoginPass;
+            }
+            else if(vm.passwordList.newLoginPass != vm.newLoginPassAgain){
                 vm.msg = '两次密码不一致';
                 return false;
             }
