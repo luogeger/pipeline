23f557690ae4a92ba649b39ebb199a8eb3d21226
kellyo0 <1281002678@qq.com>
2018-06-13 19:57:54 +0800

    欢迎页新增

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -64,8 +64,9 @@ axios.get(PATH +'/oauth/queryUserInfo').then(function (datas) {
 
     axios.get(PATH +'/oauth/queryMenu4Nav').then(function (datas) {
         $('.nav-top-panels').iTopNav(datas.data.msg);
-        // loadMainPage('.content-item', 'manage/manage.html');
-        loadMainPage('.content-item', 'manage/partnerPipeline.html');
+        // loadMainPage('.content-item', 'welcome.html');
+        loadMainPage('.content-item', 'manage/manage.html');
+        // loadMainPage('.content-item', 'manage/partnerPipeline.html');
     })
 })
 
