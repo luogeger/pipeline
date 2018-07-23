f4981cd558821dcdef30df71bfa1ea90d700290c
kellyo0 <1281002678@qq.com>
2018-07-23 19:51:41 +0800

    欢迎页弹窗add

--- static/scripts/welcome.js
+++ static/scripts/welcome.js
@@ -1,3 +1,18 @@
 /**
  * Created by User on 2018/6/13.
  */
+
+var vm = new Vue ({
+    el: '#app',
+    data: function() {
+        return {
+            dialogShow: false,
+        }
+    },
+    methods: {
+        // 关闭二级弹窗
+        hideDialog: function() {
+            this.dialogShow = true;
+        },
+    }
+})
